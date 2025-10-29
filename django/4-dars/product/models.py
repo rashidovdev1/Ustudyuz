1aa8e943c26bed0160a5367a4fd120feac5a8f0b
rashidovdev <rozibekabdurashidov590@gmail.com>
2025-10-29 16:20:58 +0500

    model

---
+++ django/4-dars/product/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
